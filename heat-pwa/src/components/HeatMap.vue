ea4e71e843be1fbb8c4e7f61c039f513be07a035
johneckert <johnteckert@gmail.com>
2018-08-31 18:51:55 -0400

    HeatMap: built and working including service worker.

---
+++ heat-pwa/src/components/HeatMap.vue
@@ -0,0 +1,25 @@
+<template>
+   <heat-map
+      :dataModel='data'
+      title='D3 Heat Map'
+      xaxis-label="date"
+      yaxis-label="volume"
+    ></heat-map>
+</template>
+<script>
+import { mapGetters } from "vuex";
+import { D3HeatMap } from "jscatalyst";
+
+export default {
+  name: "",
+  components: {
+    heatMap: D3HeatMap
+  },
+  computed: {
+    ...mapGetters(["data"])
+  },
+  methods: {}
+};
+</script>
+<style>
+</style>
--- heat-pwa/src/store.js
+++ heat-pwa/src/store.js
@@ -1,16 +1,28 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    data: [],
+    columns: []
+  },
+  getters: {
+    data: state => state.data
   },
   mutations: {
-
+    addData(state, data) {
+      state.data = data;
+    }
   },
   actions: {
-
+    fetchData({ commit }) {
+      axios.get("http://localhost:9000").then(response => {
+        commit("addData", response.data);
+        commit("generateColumns", response.data);
+      });
+    }
   }
-})
+});
