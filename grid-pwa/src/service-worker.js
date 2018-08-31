ad54a03169a6e1a3827a4e5702afbd5fe9198b91
johneckert <johnteckert@gmail.com>
2018-08-31 17:51:20 -0400

    Grid: data caches as long as you refresh oncebefore gonig offline.

--- grid-pwa/src/service-worker.js
+++ grid-pwa/src/service-worker.js
@@ -1,4 +1,10 @@
-console.log("this works!");
-
 //precaches app files
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+//caches data from ajax request using network first strategy - meaning it will only load the cache if the network is unavailable
+workbox.routing.registerRoute(
+  new RegExp("^http://localhost:9000"),
+  workbox.strategies.networkFirst({
+    cacheName: "data-cache"
+  })
+);
