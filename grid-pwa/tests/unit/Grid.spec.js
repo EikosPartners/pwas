c89e24bb2de0623db252840cdaee46ddee0b3cf4
johneckert <johnteckert@gmail.com>
2018-09-06 08:59:58 -0400

    Grid: Started adding tests

--- grid-pwa/tests/unit/Grid.spec.js
+++ grid-pwa/tests/unit/Grid.spec.js
@@ -1,20 +1,26 @@
 import { shallowMount } from "@vue/test-utils";
 import Grid from "@/components/Grid.vue";
 
-describe("Grid.vue", () => {
-  it("renders grid when data exists", () => {
-    const wrapper = shallowMount(Grid, {
-      data: [
-        {
-          date: "2017-02-08",
-          volume: 1
-        }
-      ],
-      columns: [
-        { headerName: date, field: date },
-        { headerName: volume, field: volume }
-      ]
-    });
-    expect(wrapper.text()).toMatch(msg);
-  });
+// describe("Grid.vue", () => {
+//   it("renders grid when data exists", () => {
+//     const wrapper = shallowMount(Grid, {
+//       data: [
+//         {
+//           date: "2017-02-08",
+//           volume: 1
+//         }
+//       ],
+//       columns: [
+//         { headerName: date, field: date },
+//         { headerName: volume, field: volume }
+//       ]
+//     });
+//     expect(wrapper.text()).toMatch(msg);
+//   });
+// });
+
+test("parseInt transforms date correctly", () => {
+  const input = "2018-02-20T06:10:29.000Z";
+  const output = "02-20-2018 : 06 10 29";
+  expect(this.parseDate(input)).toEqual(output);
 });
