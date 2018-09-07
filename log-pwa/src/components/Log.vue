7c5a552ade1087152e02dc9b429ab400a04232df
johneckert <johnteckert@gmail.com>
2018-09-07 15:33:23 -0400

    added an else condition for empty log

--- log-pwa/src/components/Log.vue
+++ log-pwa/src/components/Log.vue
@@ -1,48 +1,52 @@
 <template>
 
    <div style="height: 400px">
+     <p>Message Log:</p>
      <ul v-if="messages.length > 0">
        <li v-for="(msg, i) in messages" :key = "i"> {{msg}} </li>
+     </ul>
+     <ul v-else>
+       <li>Log Empty</li>
      </ul>
     </div>
 </template>
 
 <script>
-import { mapActions } from 'vuex';
+import { mapActions } from "vuex";
 export default {
-  name: 'Log',
+  name: "Log",
   data() {
     return {
       messages: []
     };
   },
   methods: {
-    ...mapActions(['fetchData']),
+    ...mapActions(["fetchData"]),
     addData(msg) {
       this.messages.unshift(msg);
     }
   },
   created() {
     const data = this.fetchData();
-    console.log('log', data);
+    console.log("log", data);
   },
   sockets: {
     connect: function() {
-      console.log('socket connected');
+      console.log("socket connected");
       this.$options.sockets.filterByDate = filter => {
-        console.log('filter', filter);
+        console.log("filter", filter);
         this.addData(filter);
       };
       this.$options.sockets.filterByProject = filter => {
-        console.log('filter', filter);
+        console.log("filter", filter);
         this.addData(filter);
       };
       this.$options.sockets.filterBySeverity = filter => {
-        console.log('filter', filter);
+        console.log("filter", filter);
         this.addData(filter);
       };
       this.$options.sockets.filterByRaisedBy = filter => {
-        console.log('filter', filter);
+        console.log("filter", filter);
         this.addData(filter);
       };
     }
