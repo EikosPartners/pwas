5118c45afeb8269dd13fdd462b937385cd0d4ff7
johneckert <johnteckert@gmail.com>
2018-09-07 14:48:31 -0400

    Heat: added tests for store methods

--- heat-pwa/src/store.js
+++ heat-pwa/src/store.js
@@ -4,30 +4,38 @@ import axios from "axios";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    data: []
-  },
-  getters: {
-    data: state => state.data
-  },
-  mutations: {
-    addData(state, data) {
-      state.data = data;
-    }
-  },
-  actions: {
-    fetchData({ commit }) {
-      axios.get("http://localhost:9000").then(response => {
-        const heatData = response.data.map(item => {
-          console.log(typeof item.date);
-          return {
-            date: item.date.split("T")[0],
-            volume: item.severity
-          };
-        });
-        commit("addData", heatData);
-      });
-    }
+const state = {
+  data: []
+};
+
+export const getters = {
+  data: state => state.data
+};
+
+export const mutations = {
+  addData(state, data) {
+    state.data = data;
   }
+};
+
+export const actions = {
+  fetchData({ commit }) {
+    axios.get("http://localhost:9000").then(response => {
+      const heatData = response.data.map(item => {
+        console.log(typeof item.date);
+        return {
+          date: item.date.split("T")[0],
+          volume: item.severity
+        };
+      });
+      commit("addData", heatData);
+    });
+  }
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations,
+  actions
 });
---
+++ heat-pwa/tests/unit/Heat.spec.js
@@ -0,0 +1,53 @@
+import { mockData } from "./mockData.js";
+import { getters, mutations, actions } from "@/store.js";
+import axios from "axios"; //imports mock axios from __mock__
+
+describe("HeatMap PWA", () => {
+  let state = mockData;
+
+  describe("Getters", () => {
+    test("data() returns data", () => {
+      const result = getters.data(state);
+      expect(result).toEqual(mockData.data);
+    });
+  });
+
+  describe("Mutations", () => {
+    test("addData() replaces state.data with fetched data", () => {
+      const newData = [
+        {
+          date: "2018-02-20T20:27:40.000Z",
+          raisedBy: 7887,
+          project: "Cotton",
+          severity: 1
+        }
+      ];
+      mutations.addData(state, newData);
+      expect(state.data).toEqual(newData);
+    });
+  });
+
+  describe("Actions", () => {
+    it("fetchData() calls axios.get", () => {
+      const commit = jest.fn();
+      actions.fetchData({ commit });
+      expect(axios.get).toBeCalled;
+    });
+
+    it("fetchData() calls commit twice", () => {
+      const commit = jest.fn();
+      async () => {
+        actions.fetchData({ commit });
+        await expect(commit.mock.calls.length).toBe(2);
+      };
+    });
+
+    it("fetchData() commits addData", () => {
+      const commit = jest.fn();
+      async () => {
+        actions.fetchData({ commit });
+        await expect(commit.mock.calls[0][0]).toBe("addData");
+      };
+    });
+  });
+});
