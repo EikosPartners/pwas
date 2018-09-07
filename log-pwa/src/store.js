314f0400924fd11234f9b0a896060dcb9bbf6e86
johneckert <johnteckert@gmail.com>
2018-09-07 15:27:17 -0400

    Log: restructured store for testing

--- log-pwa/src/store.js
+++ log-pwa/src/store.js
@@ -1,27 +1,35 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
-import axios from 'axios';
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  state: {
-    data: []
-  },
-  mutations: {
-    fetchData(state, payload) {
-      state.data = payload;
-    }
-  },
-  actions: {
-    fetchData({ commit }) {
-      axios.get('http://localhost:9000').then(res => {
-        console.log('log pwa', res);
-        commit('fetchData', res.data);
-      });
-    }
-  },
-  getters: {
-    data: state => state.data
+const state = {
+  data: []
+};
+
+export const mutations = {
+  fetchData(state, payload) {
+    state.data = payload;
   }
+};
+
+export const actions = {
+  fetchData({ commit }) {
+    axios.get("http://localhost:9000").then(res => {
+      console.log("log pwa", res);
+      commit("fetchData", res.data);
+    });
+  }
+};
+
+export const getters = {
+  data: state => state.data
+};
+
+export default new Vuex.Store({
+  state,
+  mutations,
+  actions,
+  getters
 });
