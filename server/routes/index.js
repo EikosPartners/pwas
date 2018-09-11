abf95869ad80dd2a64a8cc3a3b744ddb5bf63888
johneckert <johnteckert@gmail.com>
2018-09-11 11:48:38 -0400

    Server: added /color endpoint for fetching color settings

--- server/routes/index.js
+++ server/routes/index.js
@@ -4,13 +4,17 @@ let seed = require("../data/seed.js");
 let router = express.Router();
 
 let data = seed.generateDataSet(500);
-
+let colorObj = { action: "changeColor", color: "Blue" };
 setInterval(function() {
   data = seed.generateDataSet(100);
 }, 3600000);
 
 router.get("/", (req, res) => {
   res.send(data);
+});
+
+router.get("/color", (req, res) => {
+  res.send(colorObj);
 });
 
 module.exports = router;
