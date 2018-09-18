8ec52719f945d8f7f7fa72aa8e71a7b7f1801c26
John Eckert <johnteckert@gmail.com>
2018-09-18 14:32:33 -0400

    Pie: added glue42 to pie chart - successfully opens and filters grid

--- pie-pwa/src/components/PieChart.vue
+++ pie-pwa/src/components/PieChart.vue
@@ -8,13 +8,21 @@
 import { D3PieChart, StyleTogglerMixin } from 'jscatalyst';
 import { mapGetters, mapState, mapActions } from 'vuex';
 import jslinq from 'jslinq';
+import Messaging from '@/mixins/Messaging';
+import Windowing from '@/mixins/Windowing';
+
 
 export default {
   name: 'PieChart',
   components: {
     pieChart: D3PieChart
   },
-  mixins: [StyleTogglerMixin],
+  mixins: [StyleTogglerMixin, Messaging, Windowing],
+  data() {
+    return {
+      gridInstance: false
+    };
+  },
   computed: {
     ...mapState(['color', 'themeTrigger']),
     ...mapGetters(['data']),
@@ -50,8 +58,35 @@ export default {
       filter.dataSource = '/';
       filter.data = data.data.label;
       filter.time = new Date();
-      console.log(filter);
-      this.$socket.emit('filterByProject', filter);
+      // console.log(filter);
+
+      this.filter(filter, 'filterOnGrid');
+
+      // this.openContextWindow('Filter Results', 'http://localhost:9093', filter)
+
+      // A Named object
+      if (this.gridInstance === true) {
+        debugger;
+        // Can we pass the instance an updated context here?
+      } else {
+        let app = window.glue.appManager.application('JSCDataGrid');
+        const localWindow = window.glue.windows.my();
+        const localThis = this;
+        let windowConfig = {
+          relativeTo: localWindow.id,
+          relativePosition: 'right'
+        };
+        // let windowConfig = { }
+
+        // Launch the app and then wait for the return so that we can grab the instance Id
+        app
+          .start({ filter: filter, eventName: 'filterOnGrid' }, windowConfig)
+          .then(instance => {
+            //localThis.gridInstance = instance
+          });
+
+        this.gridInstance = true;
+      }
     },
 
     setTheme() {
@@ -72,6 +107,12 @@ export default {
         this.changeTheme(data.name);
       };
     }
+  },
+  created() {
+    this.subscribe('filterOnGrid', (context, delta, removed) => {
+      console.log('context update', context.data);
+      this.output = context.data;
+    });
   },
   watch: {
     color(newData) {
