fe6e3195b64e8edcfa0b1cb035481ad6cf4cf31a
John Eckert <johnteckert@gmail.com>
2018-09-27 13:54:33 -0400

    created subcomponents for dif filters and moved JSON parsing to mixin

--- log-pwa/src/components/CtxItem.vue
+++ log-pwa/src/components/CtxItem.vue
@@ -2,13 +2,21 @@
   <li class="context-li">
     <button @click="toggleClicked">X</button>
     <span>{{contextName}}</span>
-    <context-detail v-if="clicked" :contextName="contextName"></context-detail>
+    <div v-if="clicked">
+      <filtered-on-detail v-if="contextName === 'filterOnGrid'" :contextName="contextName"></filtered-on-detail>
+      <connect-themes-detail v-if="contextName === 'Connect.Themes'" :contextName="contextName"></connect-themes-detail>
+      <global-theme-detail v-if="contextName === 'globalTheme'" :contextName="contextName"></global-theme-detail>
+      <filtered-grid-detail v-else :contextName="contextName"></filtered-grid-detail>
+    </div>
   </li>
 </template>
 <script>
 import { mapState, mapGetters, mapActions, mapMutations } from 'vuex';
 import { StyleTogglerMixin } from 'jscatalyst';
-import contextDetail from '@/components/CtxDetail.vue'
+import filteredGridDetail from '@/components/FilteredGridDetail.vue'
+import filteredOnDetail from '@/components/FilteredOnDetail.vue'
+import connectThemesDetail from '@/components/ConnectThemesDetail.vue'
+import globalThemeDetail from '@/components/GlobalThemeDetail.vue'
 import Messaging from '@/mixins/Messaging';
 import Windowing from '@/mixins/Windowing';
 
@@ -17,7 +25,10 @@ export default {
   mixins: [Messaging, Windowing, StyleTogglerMixin],
   props: ['contextName'],
   components: {
-    contextDetail: contextDetail
+    filteredGridDetail,
+    filteredOnDetail,
+    connectThemesDetail,
+    globalThemeDetail
   },
   data() {
     return {
--- log-pwa/src/components/GlobalThemeDetail.vue
+++ log-pwa/src/components/GlobalThemeDetail.vue
@@ -6,10 +6,11 @@ import { mapState, mapGetters, mapActions, mapMutations } from 'vuex';
 import { StyleTogglerMixin } from 'jscatalyst';
 import Messaging from '@/mixins/Messaging';
 import Windowing from '@/mixins/Windowing';
+import JSONtoHTML from '@/mixins/JSONtoHTML';
 
 export default {
-  name: 'ContextDetail',
-  mixins: [Messaging, Windowing, StyleTogglerMixin],
+  name: 'globalThemeDetail',
+  mixins: [Messaging, Windowing, StyleTogglerMixin, JSONtoHTML],
   props: ['contextName'],
   data() {
     return {
@@ -17,40 +18,17 @@ export default {
       contextKey: null
     }
   },
-  computed: {
-    jsonString() {
-      return this.jsonToHTML(this.ctxInfo)
-    }
-  },
   methods: {
     toggleClicked() {
       this.clicked = !this.clicked
-    },
-    jsonToHTML(jsonObject) {
-      let keys = Object.keys(jsonObject)
-      let html = keys.map(key => {
-        // debugger
-        return this.handleKey(key, jsonObject)
-      })
-      return  html.join(' ')
-    },
-    handleKey(key, jsonObject) {
-      if (typeof jsonObject[`${key}`] !== 'object') {
-        // debugger
-        return `<div><span>${key}:</span><span>${jsonObject[`${key}`]}</span></div>`
-      }
-      else {
-        // debugger
-        return this.jsonToHTML(jsonObject[`${key}`])
-      }
     }
   },
   created() {
     this.contextKey = this.subscribe(this.contextName, (context, delta, removed) => {
       // debugger
       this.ctxInfo = context;
-    console.log("info", this.contextName)
-    console.log("info", this.ctxInfo)
+    console.log("name global theme", this.contextName)
+    console.log("info global them", this.ctxInfo)
     });
   }
 }
